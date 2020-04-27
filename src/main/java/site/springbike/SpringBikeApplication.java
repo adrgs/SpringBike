7f696d923d6be2ac0d436b8a996174da49280fa3
adrgs <albastroiudragos@gmail.com>
2020-04-28 01:56:30 +0300

    First commit

---
+++ src/main/java/site/springbike/SpringBikeApplication.java
@@ -0,0 +1,13 @@
+package site.springbike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBikeApplication.class, args);
+	}
+
+}
