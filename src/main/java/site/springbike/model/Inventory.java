b1a36537e9c7d20f21d0d1324fbdaf66a8257864
CoolShade27 <andrei.calin321@yahoo.com>
2020-04-29 14:48:07 +0300

    Class Inventory

--- src/main/java/site/springbike/model/Inventory.java
+++ src/main/java/site/springbike/model/Inventory.java
@@ -1,4 +1,102 @@
 package site.springbike.model;
 
-public class Inventory {
+import site.springbike.model.sql.Column;
+import site.springbike.model.sql.Table;
+
+import java.math.BigDecimal;
+
+@Table(name = "Inventory")
+public class Inventory implements SpringBikeModel{
+
+    @Column(name = "id",primaryKey = true)
+    private Integer id;
+
+    @Column(name = "id_company",foreignKey = true)
+    private Integer idCompany;
+
+    @Column(name = "id_client",foreignKey = true)
+    private Integer idClient;
+
+    @Column(name = "id_location",foreignKey = true,nullable = true)
+    private Integer idLocation;
+
+    @Column(name = "rent_price_hour")
+    private BigDecimal rentPriceHour;
+
+    @Column(name = "deleted",hasDefaultValue = true)
+    private Boolean deleted;
+
+    @Column(name = "visible",hasDefaultValue = true)
+    private Boolean visible;
+
+    public Inventory(Integer id, Integer idCompany, Integer idClient, Integer idLocation, BigDecimal rentPriceHour, Boolean deleted, Boolean visible) {
+        this.id = id;
+        this.idCompany = idCompany;
+        this.idClient = idClient;
+        this.idLocation = idLocation;
+        this.rentPriceHour = rentPriceHour;
+        this.deleted = deleted;
+        this.visible = visible;
+    }
+
+    public Inventory()
+    {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdCompany() {
+        return idCompany;
+    }
+
+    public void setIdCompany(Integer idCompany) {
+        this.idCompany = idCompany;
+    }
+
+    public Integer getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(Integer idClient) {
+        this.idClient = idClient;
+    }
+
+    public Integer getIdLocation() {
+        return idLocation;
+    }
+
+    public void setIdLocation(Integer idLocation) {
+        this.idLocation = idLocation;
+    }
+
+    public BigDecimal getRentPriceHour() {
+        return rentPriceHour;
+    }
+
+    public void setRentPriceHour(BigDecimal rentPriceHour) {
+        this.rentPriceHour = rentPriceHour;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Boolean getVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
 }
