4b5323f3a612b83923e22be8989703a9c62e682e
adrgs <albastroiudragos@gmail.com>
2020-05-15 02:53:57 +0300

    Added POST logic to LoginController

--- src/main/java/site/springbike/controller/account/LoginController.java
+++ src/main/java/site/springbike/controller/account/LoginController.java
@@ -3,12 +3,27 @@ package site.springbike.controller.account;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
     @GetMapping("/account/login")
-    public String login(Model model) {
+    public String getLogin(Model model) {
         model.addAttribute("title", "Login");
         return "account/login";
+    }
+
+    @PostMapping("/account/login")
+    public ModelAndView postLogin(HttpServletRequest request, HttpServletResponse response) {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+
+        return new ModelAndView("redirect:/index"); //success - to be modified to /dashboard or something
     }
 }
