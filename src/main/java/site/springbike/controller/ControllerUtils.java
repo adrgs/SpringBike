08e2ecadbdc776e69e41941223843e89781a74f7
CoolShade27 <andrei.calin321@yahoo.com>
2020-05-15 14:58:09 +0300

    Added parseModelFromInput

--- src/main/java/site/springbike/controller/ControllerUtils.java
+++ src/main/java/site/springbike/controller/ControllerUtils.java
@@ -1,13 +1,18 @@
 package site.springbike.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.servlet.ModelAndView;
 import site.springbike.cache.UserCacheManager;
+import site.springbike.model.SpringBikeModel;
 import site.springbike.model.User;
+import site.springbike.model.sql.Column;
 import site.springbike.session.SessionUtils;
 import site.springbike.session.UserSession;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Field;
+import java.util.Map;
 
 public class ControllerUtils {
     public static ModelAndView errorModelAndView(String view, String title, String error) {
@@ -37,6 +42,46 @@ public class ControllerUtils {
         } catch (Exception ex) {
             return null;
         }
+    }
+
+    public boolean parseModelFromInput(SpringBikeModel model, Map<String, String[]> map){
+
+        Class<?> myClass = model.getClass();
+        if (myClass.getSuperclass() != null) {
+            for (Field field : myClass.getSuperclass().getDeclaredFields()) {
+                field.setAccessible(true);
+                Column column = field.getAnnotation(Column.class);
+                if (column == null) continue;
+
+                String[] val = map.get(column.name());
+                if (val == null) continue;
+                if (val.length != 1 && !column.nullable()) {
+                    return false;
+                }
+
+                try {
+                    field.set(model, val[0]);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        for (Field field : myClass.getDeclaredFields()) {
+            Column column = field.getAnnotation(Column.class);
+            field.setAccessible(true);
+            String[] val = map.get(column.name());
+            if (val == null) continue;
+            if (val.length != 1 && !column.nullable()) {
+                return false;
+            }
+
+            try {
+                field.set(model, val[0]);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
     }
 
 }
