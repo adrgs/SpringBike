99203338de3f4875e67164198a387f7bfd5b061b
adrgs <albastroiudragos@gmail.com>
2020-05-15 14:30:01 +0300

    Added checkAuthentication method in ControllerUtils and the session is now set in LoginController after a successful login

--- src/main/java/site/springbike/controller/account/LoginController.java
+++ src/main/java/site/springbike/controller/account/LoginController.java
@@ -5,9 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import site.springbike.cache.UserCacheManager;
 import site.springbike.model.User;
 import site.springbike.repository.UserRepository;
+import site.springbike.session.SessionUtils;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -31,6 +34,17 @@ public class LoginController {
             modelAndView.addObject("title", "Login");
             modelAndView.addObject("error", "The credentials you entered did not match our records. Please double-check and try again.");
             return modelAndView;
+        }
+
+        UserCacheManager.getInstance().putUser(user);
+        try {
+            String session = SessionUtils.getInstance().encryptSession(user.getId());
+            Cookie cookie = new Cookie("session", session);
+            cookie.setPath("/");
+            cookie.setMaxAge(129600);
+            response.addCookie(cookie);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
 
         return new ModelAndView("redirect:/index"); //success - to be modified to /dashboard or something
